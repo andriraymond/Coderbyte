7ec01cee43ec008ce49972ec94913ebab5f78ff1
andriraymond <andriraymondo@gmail.com>
2024-06-15 23:38:26 +0700

    update react button

---
+++ Coderbyte ~ Easy/04. React Button Toggle.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+
+function Toggle() {
+  const [isOn, setIsOn] = useState(true);
+
+  function handleClick() {
+    setIsOn(!isOn);
+  }
+
+  return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Toggle />);
